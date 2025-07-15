3783507a2e5ee1a4a5195681ef27e7a7bb55c59f
German B <german.bejaranoc@gmail.com>
2025-07-15 09:58:11 -0600

    Experimenting with templates and navigation

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    return render_template('home.html')  #'Hello, Flask!'
+
+@app.route('/about')
+def about():
+    return render_template('about.html') #'This is the about page.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-	
+
+
